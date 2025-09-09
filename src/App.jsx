bbe6847d761cd49d5180381de508f832ae48c81a
Divyanshguptaj <luckys510039@gmail.com>
2025-09-09 15:04:48 +0530

    updated toggle mode dark/light

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Navbar from './components/Navbar';
-import Leo9Homepage from './components/landingpage';
+import Leo9Homepage from './components/Landingpage';
 import './App.css'
 
 const App = () => {
